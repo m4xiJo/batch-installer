f4988820552b93a7e257856877b159d2916c9f9d
Max J <14187946+m4xiJo@users.noreply.github.com>
2020-06-24 14:35:23 +0200

    initial

---
+++ bulkexecute.bat
@@ -0,0 +1,67 @@
+@echo off
+openfiles > nul 2>&1
+if not %errorlevel% equ 0 (
+  echo You didn't run this batch file as Administrator! Please try again... Exiting in 5 seconds...
+  timeout 5 > nul
+  exit /b 1
+)
+
+set wtitle=Bulk Install
+pushd %~dp0
+::cd %~dp0
+set ext=*.exe *.msi
+set quietfolder=\quietinstall
+set /a count=0
+set /a current=0
+set /a progress=0
+
+setlocal enabledelayedexpansion
+
+for /r %%x in (%ext%) do set /a count+=1
+
+title !progress!%% %wtitle% !current! of %count%
+
+echo RECURSIVELY RUNNING FILES...
+echo.
+
+for /f %%f in ('dir /s /b /a:-d %ext% ^| findstr /v /i /c:"\%quietfolder%\\"') do (
+	echo Starting file: %%f
+	start /wait "wtitle" "%%f"
+	
+	if %errorlevel% equ 0 (
+		echo File %%~nf returned no errors
+		echo.
+	) else (
+		echo File %%~nf exited with error code %errorlevel%
+		echo.
+	)
+
+	set /a current+=1
+	set /a progress=100*current/count
+	title !progress!%% %wtitle% !current! of %count%
+)
+
+echo RECURSIVELY AND QUIETLY INSTALLING PACKAGES IN %quietfolder%
+echo.
+
+for /r %%f in (%quietfolder%%ext%) do (
+	echo Installing %%f...
+    	start /wait msiexec /i "%%f" /qn
+
+	if %errorlevel% equ 0 (
+		echo File %%~nf returned no errors
+		echo.
+	) else (
+		echo File %%~nf exited with code %errorlevel%
+		echo.
+	)
+
+	set /a current+=1
+	set /a progress=100*current/count
+	title !progress!%% %wtitle% !current! of %count%
+)
+
+endlocal
+popd
+echo Sequence complete, press any key to exit!
+pause > nul
